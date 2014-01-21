a07b7b8d5ba58dc94649ef16e4e2d376ddb83ea0
Lucas Arruda <larruda@avenuecode.com>
2014-01-21 15:12:14 -0200

    Fixing paths

--- src/main.js
+++ src/main.js
@@ -1,27 +1,25 @@
 require.config({
   paths: {
-    jquery: '../lib/js/jquery/jquery-min',
-    backbone: '../lib/js/backbone/backbone-min',
-    underscore: '../lib/js/underscore/underscore-min',
-    commonjs: '../lib/js/commonjs/common-min'
+    jquery: '../lib/js/jquery/jquery.min',
+    backbone: '../lib/js/backbone/backbone.min',
+    underscore: '../lib/js/underscore/underscore.min',
+    commonjs: '../lib/js/commonjs/common.min'
   },
-    shim: {
-        "underscore": {
-            deps: [],
-            exports: "_"
-        },
-        "backbone": {
-            deps: ["jquery", "underscore"],
-            exports: "Backbone"
-        },
+  shim: {
+    "underscore": {
+      deps: [],
+      exports: "_"
+    },
+    "backbone": {
+      deps: ["jquery", "underscore"],
+      exports: "Backbone"
     }
+  }
 });
-require(['jquery', 'backbone', 'underscore', 'commonjs'], function (app, $) {
-  'use strict';
-  function ($, _, Backbone) {
+
+require(['jquery', 'underscore', 'backbone', 'commonjs'], function ($, _, Backbone) {
     console.log("Test output");
     console.log("$: " + typeof $);
     console.log("_: " + typeof _);
     console.log("Backbone: " + typeof Backbone);
-  }
 });
